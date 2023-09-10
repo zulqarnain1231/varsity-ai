41eb1715b00c663202b5b34e99d04884923ebc33
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-09 20:00:44 -0700

    finished the animations

--- components/Home/Pricing/FreeCard.tsx
+++ components/Home/Pricing/FreeCard.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Button from "@/components/Shared/Buttons/Button";
 import React from "react";
 import { BsCheckLg } from "react-icons/bs";
+import { Fade, Slide } from "react-awesome-reveal";
 
 const FreeCard = () => {
   const freeFeatures = [
@@ -9,36 +11,40 @@ const FreeCard = () => {
     "Regular Updates",
   ];
   return (
-    <div className="md:w-[80%] w-full flex flex-col items-start justify-start gap-10 py-8 px-12 bg-gradient-to-br from-brand-main to-brand-secondary rounded-[30px]">
-      <div className="w-full flex flex-col items-start justify-start gap-4">
-        <h3 className="text-white text-2xl font-bold">Free </h3>
-        <p className="text-lg text-white/80 font-normal">
-          Perfect for getting started.
-        </p>
+    <Slide className="w-full" triggerOnce direction="up">
+      <div className="md:w-[80%] w-full flex flex-col items-start justify-start gap-10 py-8 px-12 bg-gradient-to-br from-brand-main to-brand-secondary rounded-[30px]">
+        <div className="w-full flex flex-col items-start justify-start gap-4">
+          <h3 className="text-white text-2xl font-bold">Free </h3>
+          <p className="text-lg text-white/80 font-normal">
+            Perfect for getting started.
+          </p>
+        </div>
+        <div className="w-full flex flex-col items-start justify-start gap-4">
+          <h2 className="text-white text-[56px] leading-[64px] font-bold">
+            $0
+          </h2>
+          <p className="text-white/80 text-lg font-normal">free forever</p>
+        </div>
+        <hr className="w-full h-[2px] text-white/30" />
+        <div className="w-full flex flex-col items-start justify-start gap-4">
+          {freeFeatures.map((item: any, index: number) => (
+            <div
+              key={index}
+              className="w-full flex items-center justify-start gap-2"
+            >
+              <BsCheckLg className="text-white text-2xl" />
+              <p className="text-lg text-white font-medium ">{item}</p>
+            </div>
+          ))}
+        </div>
+        <Button
+          text="Join Waitlist"
+          route="https://tally.so/r/nP14xb"
+          styles="bg-white text-brand-tartary w-full h-[50px] hover:bg-white/60 hover:duration-300"
+          shadow="shadow-lg"
+        />
       </div>
-      <div className="w-full flex flex-col items-start justify-start gap-4">
-        <h2 className="text-white text-[56px] leading-[64px] font-bold">$0</h2>
-        <p className="text-white/80 text-lg font-normal">free forever</p>
-      </div>
-      <hr className="w-full h-[2px] text-white/30" />
-      <div className="w-full flex flex-col items-start justify-start gap-4">
-        {freeFeatures.map((item: any, index: number) => (
-          <div
-            key={index}
-            className="w-full flex items-center justify-start gap-2"
-          >
-            <BsCheckLg className="text-white text-2xl" />
-            <p className="text-lg text-white font-medium ">{item}</p>
-          </div>
-        ))}
-      </div>
-      <Button
-        text="Join Waitlist"
-        route="https://tally.so/r/nP14xb"
-        styles="bg-white text-brand-tartary w-full h-[50px] hover:bg-white/60 hover:duration-300"
-        shadow="shadow-lg"
-      />
-    </div>
+    </Slide>
   );
 };
 
--- components/Home/Pricing/ProCard.tsx
+++ components/Home/Pricing/ProCard.tsx
@@ -1,7 +1,8 @@
+"use client";
 import Button from "@/components/Shared/Buttons/Button";
 import React from "react";
 import { BsCheckLg } from "react-icons/bs";
-
+import { Fade, Slide } from "react-awesome-reveal";
 const ProCard = () => {
   const proFeatures = [
     "Unlimited Papers",
@@ -11,38 +12,42 @@ const ProCard = () => {
     "Early Access to Updates",
   ];
   return (
-    <div className="md:w-[80%] w-full flex flex-col items-start justify-start gap-10 py-10 px-12 relative rounded-[30px]  pro-card shadow-yellow">
-      <div className="w-full flex flex-col items-start justify-start gap-4">
-        <h3 className="text-black-main/70 text-2xl font-bold">Pro </h3>
-        <p className="text-lg text-black-main/70 font-normal">Full featured</p>
+    <Slide triggerOnce className="w-full" direction="up">
+      <div className="md:w-[80%] w-full flex flex-col items-start justify-start gap-10 py-10 px-12 relative rounded-[30px]  pro-card shadow-yellow">
+        <div className="w-full flex flex-col items-start justify-start gap-4">
+          <h3 className="text-black-main/70 text-2xl font-bold">Pro </h3>
+          <p className="text-lg text-black-main/70 font-normal">
+            Full featured
+          </p>
+        </div>
+        <div className="w-full flex flex-col items-start justify-start gap-4">
+          <h2 className="text-brand-tartary text-[56px] leading-[64px] font-bold">
+            $10
+          </h2>
+          <p className="text-black-main/60 text-lg font-normal">
+            per member/per month
+          </p>
+        </div>
+        <hr className="w-full h-[2px] text-white/30" />
+        <div className="w-full flex flex-col items-start justify-start gap-4">
+          {proFeatures.map((item: any, index: number) => (
+            <div
+              key={index}
+              className="w-full flex items-center justify-start gap-2"
+            >
+              <BsCheckLg className="text-brand-tartary text-2xl" />
+              <p className="text-lg text-black-main font-medium ">{item}</p>
+            </div>
+          ))}
+        </div>
+        <Button
+          route="https://tally.so/r/nP14xb"
+          text="Join Waitlist"
+          styles="bg-gradient-to-r from-brand-main to-brand-secondary text-white cursor-pointer text-brand-tartary w-full h-[50px] after:absolute after:h-full after:rounded-[17px] after:w-0 after:left-0 after:top-0 after:z-10 after:bg-white/30  hover:after:w-full hover:after:duration-300"
+          shadow="shadow-main"
+        />
       </div>
-      <div className="w-full flex flex-col items-start justify-start gap-4">
-        <h2 className="text-brand-tartary text-[56px] leading-[64px] font-bold">
-          $10
-        </h2>
-        <p className="text-black-main/60 text-lg font-normal">
-          per member/per month
-        </p>
-      </div>
-      <hr className="w-full h-[2px] text-white/30" />
-      <div className="w-full flex flex-col items-start justify-start gap-4">
-        {proFeatures.map((item: any, index: number) => (
-          <div
-            key={index}
-            className="w-full flex items-center justify-start gap-2"
-          >
-            <BsCheckLg className="text-brand-tartary text-2xl" />
-            <p className="text-lg text-black-main font-medium ">{item}</p>
-          </div>
-        ))}
-      </div>
-      <Button
-        route="https://tally.so/r/nP14xb"
-        text="Join Waitlist"
-        styles="bg-gradient-to-r from-brand-main to-brand-secondary text-white cursor-pointer text-brand-tartary w-full h-[50px] after:absolute after:h-full after:rounded-[17px] after:w-0 after:left-0 after:top-0 after:z-10 after:bg-white/30  hover:after:w-full hover:after:duration-300"
-        shadow="shadow-main"
-      />
-    </div>
+    </Slide>
   );
 };
 
