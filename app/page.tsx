4aba12634dcef083c71d69fa5d85633c7658f970
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-09 19:41:10 -0700

    added slide in effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import HomePage from "@/components/Home/HomePage";
 export default function Home() {
   return (
-    <main className="w-full h-full">
+    <main className="w-full h-full overflow-hidden">
       <HomePage />
     </main>
   );
--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -1,10 +1,11 @@
+"use client";
 import React from "react";
 import ComponentWrapper from "../Shared/Wrappers/ComponentWrapper";
 import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import Button from "../Shared/Buttons/Button";
-
+import { Fade, Slide } from "react-awesome-reveal";
 const HeroSection = () => {
   return (
     <ComponentWrapper
@@ -12,32 +13,36 @@ const HeroSection = () => {
       style="w-full min-h-[calc(100vh-80px)] flex items-center py-10 lg:py-0"
     >
       <div className="w-full h-full grid lg:grid-cols-2 lg:gap-0 gap-8 place-items-center">
-        <aside className="sm:w-[70%] w-full flex flex-col items-start justify-start gap-4">
-          <h1 className="text-5xl text-black-main font-bold">
-            Your AI Academic Research Assistant
-          </h1>
-          <p className="text-lg text-black-main/70 font-normal">
-            VarsityAI is a tool to help academic researchers save hundreds of
-            hours by eliminating endless manual searches to find relevant
-            information. Our AI answers complicated queries in seconds
-            simplifying the process of digesting complex academic literature,
-            making research workflow more efficient and collaborative.{" "}
-          </p>
-          <Button
-            route="https://tally.so/r/nP14xb"
-            text="Join Waitlist"
-            shadow="shadow-main"
-            icon
-          />
-        </aside>
-        <aside className="lg:w-full sm:w-[70%] w-full h-[490px] relative">
-          <Image
-            className="w-full h-full object-cover"
-            src={"/Assets/Hero.webp"}
-            alt=""
-            fill
-          />
-        </aside>
+        <Slide direction="up" triggerOnce className="w-full">
+          <aside className="sm:w-[70%] w-full flex flex-col items-start justify-start gap-4">
+            <h1 className="text-5xl text-black-main font-bold">
+              Your AI Academic Research Assistant
+            </h1>
+            <p className="text-lg text-black-main/70 font-normal">
+              VarsityAI is a tool to help academic researchers save hundreds of
+              hours by eliminating endless manual searches to find relevant
+              information. Our AI answers complicated queries in seconds
+              simplifying the process of digesting complex academic literature,
+              making research workflow more efficient and collaborative.{" "}
+            </p>
+            <Button
+              route="https://tally.so/r/nP14xb"
+              text="Join Waitlist"
+              shadow="shadow-main"
+              icon
+            />
+          </aside>
+        </Slide>
+        <Fade triggerOnce className="w-full">
+          <aside className="lg:w-full sm:w-[70%] w-full h-[490px] relative">
+            <Image
+              className="w-full h-full object-cover"
+              src={"/Assets/Hero.webp"}
+              alt=""
+              fill
+            />
+          </aside>
+        </Fade>
       </div>
     </ComponentWrapper>
   );
